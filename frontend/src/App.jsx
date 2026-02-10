1e30426083cd7369ab9261f60c81858d81ed69ae
Vihanga Kulathilake <vihangajanith12m@gmail.com>
2026-02-10 11:15:17 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,21 @@
 import React from 'react';
-import AdminNavbar from './components/common/AdminNavbar';
-import UserNavbar from './components/common/UserNavbar';
-import Sidebar from './components/common/Sidebar';
-import Footer from './components/common/Footer';
-import BoardingCard from './components/boarding/BoardingCard';
-import BoardingForm from './components/boarding/BoardingForm';
-import TenantForm from './components/tenant/TenantForm';
-import PaymentTable from './components/payment/PaymentTable';
-import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landing/LandingPage';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
 
 function App() {
     return (
-        <div className="d-flex flex-column min-vh-100 bg-light">
-            <div className="mb-4">
-                <UserNavbar />
-            </div>
-
-            {/* <Navbar /> (Removed in favor of distinct examples) */}
-            {/* <div className="d-flex flex-grow-1">
-                <Sidebar />
-                <Container className="p-4">
-                    <h1>Dashboard</h1>
-                    <Row className="mb-4">
-                        <Col>
-                            <h2>Boarding</h2>
-                            <div className="d-flex gap-3 flex-wrap">
-                                <BoardingCard />
-                                <BoardingCard />
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row className="mb-4">
-                        <Col md={6}>
-                            <h2>Add Boarding</h2>
-                            <BoardingForm />
-                        </Col>
-                        <Col md={6}>
-                            <h2>Add Tenant</h2>
-                            <TenantForm />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <h2>Payments</h2>
-                            <PaymentTable />
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-            <Footer /> */}
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Register />} />
+            </Routes>
+        </Router>
     );
 }
 

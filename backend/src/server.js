41892f3300588b1f8603729c5ba6176ca6097399
Vihanga Kulathilake <vihangajanith12m@gmail.com>
2026-03-17 16:11:22 +0530

    Update the server with room routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import boardingRoutes from "./routes/boardingRoutes.js";
+import roomRoutes from "./routes/roomRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/boardings", boardingRoutes);
+app.use("/api/rooms", roomRoutes);
 
 app.get("/", (req, res) => {
   res.send("API running");
